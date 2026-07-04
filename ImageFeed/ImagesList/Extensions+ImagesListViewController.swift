b8d3d287c5b07960d0bc632544f74af371ab1644
Adilkhan <dfw.adilhan@gmail.com>
2026-07-04 19:27:19 +0600

    добавлена кастомная ячейка

---
+++ ImageFeed/ImagesList/Extensions+ImagesListViewController.swift
@@ -0,0 +1,10 @@
+//
+//  Extension+ImagesListViewController.swift
+//  ImageFeed
+//
+//  Created by Adilkhan on 4/7/26.
+//
+
+import UIKit
+
+
---
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -0,0 +1,12 @@
+//
+//  ImagesListCell.swift
+//  ImageFeed
+//
+//  Created by Adilkhan on 4/7/26.
+//
+
+import UIKit
+
+final class ImagesListCell: UITableViewCell {
+    static let reuseIdentifier = "ImagesListCell"
+}
