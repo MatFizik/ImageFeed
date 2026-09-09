5814404c60b9c4f75fcf0c6cc05a422bccfe0e6a
Adilkhan <dfw.adilhan@gmail.com>
2026-09-09 23:57:04 +0600

    sharebtn и bottomsheet

---
+++ ImageFeed/SingleImageView/Extension+SingleImageViewController.swift
@@ -0,0 +1,14 @@
+//
+//  Extension+SingleImageViewController.swift
+//  ImageFeed
+//
+//  Created by Adilkhan on 2/9/26.
+//
+
+import UIKit
+
+extension SingleImageViewController: UIScrollViewDelegate {
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
+}
--- ImageFeed/SingleImageView/SingleImageViewController.swift
+++ ImageFeed/SingleImageView/SingleImageViewController.swift
@@ -9,14 +9,50 @@ import UIKit
 final class SingleImageViewController: UIViewController {
     var image: UIImage?
     
-    @IBOutlet private var imageView: UIImageView!
+    @IBOutlet final var imageView: UIImageView!
+    @IBOutlet private var scrollView: UIScrollView!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         imageView.image = image
+        
+        scrollView.minimumZoomScale = 0.1
+        scrollView.maximumZoomScale = 1.25
+        
+        imageView.image = image
+        
+        guard let image else { return }
+        imageView.frame.size = image.size
+        
+        rescaleAndCenterImageInScrollView(image: image)
     }
     
+    private func rescaleAndCenterImageInScrollView(image: UIImage) {
+        let minZoomScale = scrollView.minimumZoomScale
+        let maxZoomScale = scrollView.maximumZoomScale
+        view.layoutIfNeeded()
+        let visibleRectSize = scrollView.bounds.size
+        let imageSize = image.size
+        let hScale = visibleRectSize.width / imageSize.width
+        let vScale = visibleRectSize.height / imageSize.height
+        let scale = min(maxZoomScale, max(minZoomScale, min(hScale, vScale)))
+        scrollView.setZoomScale(scale, animated: false)
+        scrollView.layoutIfNeeded()
+        let newContentSize = scrollView.contentSize
+        let x = (newContentSize.width - visibleRectSize.width) / 2
+        let y = (newContentSize.height - visibleRectSize.height) / 2
+        scrollView.setContentOffset(CGPoint(x: x, y: y), animated: false)
+    }
+    
+    @IBAction func didTabShareButton(_ sender: Any) {
+        guard let image else { return }
+        let share = UIActivityViewController(
+            activityItems: [image],
+            applicationActivities: nil
+        )
+        present(share, animated: true, completion: nil)
+    }
     @IBAction func didTapBackButton(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
