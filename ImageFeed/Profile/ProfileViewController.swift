862c481d54f26c740e867b85385cd14dd49af1ad
Adilkhan <dfw.adilhan@gmail.com>
2026-09-22 00:17:01 +0600

    подключение kingfisher и вывод картинки

--- ImageFeed/Profile/ProfileViewController.swift
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -5,6 +5,7 @@
 //  Created by Adilkhan on 1/9/26.
 //
 import UIKit
+import Kingfisher
 
 final class ProfileViewController: UIViewController {
     
@@ -70,8 +71,39 @@ final class ProfileViewController: UIViewController {
     private func updateAvatar() {
         guard
             let profileImageURL = ProfileImageService.shared.avatarURL,
-            let url = URL(string: profileImageURL)
+            let imageUrl = URL(string: profileImageURL)
         else { return }
+        
+        print("imageUrl: \(imageUrl)")
+        
+        let placeholderImage = UIImage(systemName: "person.circle.fill")?
+            .withTintColor(.lightGray, renderingMode: .alwaysOriginal)
+            .withConfiguration(UIImage.SymbolConfiguration(pointSize: 70, weight: .regular, scale: .large))
+        
+        let processor = RoundCornerImageProcessor(cornerRadius: 35)
+        avatarImageView.kf.indicatorType = .activity
+        avatarImageView.kf.setImage(
+            with: imageUrl,
+            placeholder: placeholderImage,
+            options: [
+                .processor(processor),
+                .scaleFactor(UIScreen.main.scale),
+                .cacheOriginalImage,
+                .forceRefresh
+            ]) { result in
+                
+                switch result {
+                case .success(let value):
+                    print(value.image)
+                    
+                    print(value.cacheType)
+                    
+                    print(value.source)
+                    
+                case .failure(let error):
+                    print("Ошибка получения аватарки: \(error.localizedDescription)")
+                }
+            }
     }
        
     
