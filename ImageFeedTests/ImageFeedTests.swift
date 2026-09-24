d6868d7aba968aa6c7b6a4415657eec05633aa2f
Adilkhan <dfw.adilhan@gmail.com>
2026-09-24 15:26:24 +0600

    refactor: удалил тест на получение картинок

--- ImageFeedTests/ImageFeedTests.swift
+++ ImageFeedTests/ImageFeedTests.swift
@@ -21,20 +21,4 @@ final class ImageFeedTests: XCTestCase {
 }
 
 final class ImagesListServiceTests: XCTestCase {
-    func testFetchPhotos() {
-        let service = ImageListService.shared
-        
-        let expectation = self.expectation(description: "Wait for Notification")
-        NotificationCenter.default.addObserver(
-            forName: ImageListService.didChangeNotification,
-            object: nil,
-            queue: .main) { _ in
-                expectation.fulfill()
-            }
-        
-        service.fetchPhotosNextPage()
-        wait(for: [expectation], timeout: 10)
-        
-        XCTAssertEqual(service.photos.count, 10)
-    }
 }
